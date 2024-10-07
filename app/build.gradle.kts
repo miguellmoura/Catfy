4118d6457dc91416d6a47b1fcf50a2aeabb0ed65
ymandy1 <amanda.study110@gmail.com>
2024-10-06 22:18:03 -0300

    add home page and initialize recyclerview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/example/catify/MainActivity.kt
+++ app/src/main/java/com/example/catify/MainActivity.kt
@@ -1,11 +1,22 @@
-package com.example.catify
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.catify.MyItem
+import com.example.catify.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main_page)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        val myItemList = listOf(
+            MyItem.MyItem("Item 1", R.drawable.gatinho_icon),
+            MyItem.MyItem("Item 2", R.drawable.gatinho_icon),
+        )
     }
-}
+    }
